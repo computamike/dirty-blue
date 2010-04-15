0d48eb8717d6a79e4353692ad88edaa00ed6b3ee
themedropbox <themedropbox@8c925393-aaf8-4f5c-984e-1f1965effe94>
2010-04-15 02:43:40 +0000

    New theme: Dirty Blue - 1.0

---
+++ 1.0/script/fader.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+/* nav bar */
+	$('#nav div').hide();
+	$('#page').fadeIn('normal');
+		
+	$('.navswitch').click(function() {
+		var thisClass = this.className.slice(0,2);
+		$('.tab').hide();
+		$('.' + thisClass).fadeIn();
+		return false;
+	});
+
+	$('#content').click(function() {
+		$('.tab').hide();
+	});	
+/* comments */
+	$('#comment-form ul li p').hide();
+	$('#comment-form ul li p:last').slideDown();
+	
+	$('.commentmetadata').click(function() {
+		$(this).next('p').slideToggle('slow');
+		return false;
+	});
+	
+	$('#comment-toggle').click(function() {
+		$('#comment-form ul li p').slideToggle('slow');
+		return false;
+	});
+	
+	
+		
+});
+
+
---
+++ 1.0/sidebar.php
@@ -0,0 +1,28 @@
+	  </div>
+      <div id="rightcolumn">
+	    <div><a id="rss-right" href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Subscribe to this site'); ?>"></a></div>
+		<h3>Categories</h3>
+		  <ul>
+			<?php wp_list_cats('sort_column=name&optioncount=0'); ?>
+		  </ul>
+
+		<h3>Blogroll</h3>
+		  <ul>
+			<?php get_links(-1, '<li>', '</li>', ' - '); ?>
+		  </ul>
+		  
+		<h3>Archives</h3>
+		  <ul>
+			<?php wp_get_archives('type=monthly&show_post_count=0'); ?>
+		  </ul>
+		  
+		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
+		<?php endif; ?>
+	
+		<div id="ads-right">
+		  <a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/ad125.jpg" alt="Ad 1" /></a><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/ad125.jpg" alt="Ad 2" /></a>
+		  <a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/ad125.jpg" alt="Ad 3" /></a><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/ad125.jpg" alt="Ad 4" /></a>
+	    </div>
+	  </div>
+	  <div class="clear"></div>
+	</div>
